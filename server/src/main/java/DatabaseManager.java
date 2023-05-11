ee65a130d4555611d629e7e28aae9c06d119a14b
Łukasz Skowronski <lukasz.skowronski83@wp.pl>
2023-05-11 10:25:01 +0200

    Update file DataBaseManager.java

--- server/src/main/java/DatabaseManager.java
+++ server/src/main/java/DatabaseManager.java
@@ -1,19 +1,18 @@
 import java.sql.*;
 
 public class DatabaseManager {
-//    private static final String DB_URL = "jdbc:postgresql://localhost:5432/your_database";
+
     private static final String DB_SERVER = "jdbc:mysql://localhost:3306/?useSSL=false";
     private static final String USER = "root";
     private static final String PASS = "DataStore1root@";
+    private static final String query = "SELECT v.brand, v.model, io.insurer, io.price, io.insurance_options "
+            + "FROM insurance_db.vehicles v "
+            + "JOIN insurance_db.insurance_offers io ON v.id = io.vehicle_id "
+            + "JOIN insurance_db.users u ON v.login = u.login "
+            + "WHERE u.id = ?";
 
     public static String getVehicleInsuranceOffers(String userId) throws SQLException {
-        String query = "SELECT v.brand, v.model, io.insurer, io.price, io.insurance_options "
-                + "FROM insurance_db.vehicles v "
-                + "JOIN insurance_db.insurance_offers io ON v.id = io.vehicle_id "
-                + "JOIN insurance_db.users u ON v.login = u.login "
-                + "WHERE u.id = ?";
 
-//        String query = "SELECT id, nick FROM insurance_db.users WHERE users.id = ?";
 
         StringBuilder result = new StringBuilder();
 
